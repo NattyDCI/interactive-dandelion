bf83ff45ea4ed2b222815422de756cd92dfacee4
NathaliaPadron <nbavutti@gmail.com>
2026-03-06 14:28:26 +0100

    changes

--- letter.js
+++ letter.js
@@ -1,37 +1,11 @@
-class letter {
+class Letter {
   constructor(x, y, fontSize) {
     this.position = createVector(x, y); //vector is a value that can hold 2-3 values
-    let alphabets = [
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-      "N",
-      "O",
-      "P",
-      "Q",
-      "R",
-      "S",
-      "T",
-      "U",
-      "V",
-      "W",
-      "X",
-      "Y",
-      "Z",
-    ];
+    let alphabets = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     this.letter = random(alphabets);
     this.angle = 0;
     this.angleV = 0.1;
+    this.fontSize = fontSize;
   }
   update() {
     this.angle += this.angleV; //updating the angle wich is angle velocity
@@ -44,7 +18,6 @@ class letter {
     textSize(this.fontSize);
     textAlign(CENTER, CENTER); //centers the ellipse around the center of the p
     text(this.letter, 0, 0);
-    ellipse(width / 2, height / 2, 10, 10);
     pop();
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,14 +1,33 @@
+let letters = [];
+let rings = [];
+let ringsnum = 3;
+
 function setup() {
   createCanvas(400, 400);
+
+  let r = 100; 
+  let circumference = 2 * PI * r;
+  let fontSize = 10;
+  let num = floor(circumference / fontSize);
+
+  for (let i=0; i<num; i++){
+    let angle = TWO_PI / num * i; //this will give me the space between each letter and multiplied by i to give me another size each round.// 
+    
+    // para sacar el angulo necesitamos trigonometria    
+    let x = width/2 + r * cos(angle);
+    let y = height/2 + r * sin(angle);
+    letters.push(new Letter(x, y, 50));
+
+  }
+
+  
 }
 
 function draw() {
-  background("bluesteel");
-  textSize(32);
-  fill(255);
-  text("hi", 200, 200);
-  stroke(255);
-  strokeWidth(4);
+  background("steelblue");
 
-  circle(40, 40, 20);
+  for(let i=0; i<letters.length; i++) {
+    letters[i].update();
+    letters[i].display();
+  }
 }

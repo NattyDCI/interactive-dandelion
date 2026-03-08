ec98f984977faa6fea2defd0b121459e246ebe2d
NathaliaPadron <nbavutti@gmail.com>
2026-03-08 01:46:54 +0100

    finished dandelion

--- letter.js
+++ letter.js
@@ -1,20 +1,35 @@
 class Letter {
-  constructor(x, y, fontSize) {
-    this.position = createVector(x, y); //vector is a value that can hold 2-3 values
+  constructor(x, y, fontSize, font) {
     let alphabets = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     this.letter = random(alphabets);
+    this.font = font;
     this.angle = 0;
-    this.angleV = 0;
+    this.position = createVector(x, y); //vector is a value that can hold 2-3 values
+    this.velocity = createVector(0, 0)
+    this.acceleration = createVector(0, 0);
     this.fontSize = fontSize;
+    this.angleV = 0;  // angle velocity
   }
   update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
     this.angle += this.angleV; //updating the angle wich is angle velocity
+
+  }
+
+  applyForce(force) {
+    this.acceleration.add(force);
+
   }
 
   display() {
+    fill(255);
+    noStroke();
     push();
     translate(this.position.x, this.position.y);
     rotate(this.angle);
+    textFont(this.font);
     textSize(this.fontSize);
     textAlign(CENTER, CENTER); //centers the ellipse around the center of the p
     text(this.letter, 0, 0);
--- sketch.js
+++ sketch.js
@@ -1,16 +1,47 @@
-let rings = [];
-let numRing = 3;
-let letters = [];
 
-function setup() {
-  createCanvas(400, 400);
+
+//ml5 variables
+let faceMesh;
+let video;
+let faces = [];
+let options = { maxFaces: 1, refineLandmarks: false, flipped: true };
+let previousLipDistance;
+
+//letter variables
+let rings = [];
+let numRing = 5;
+let fonts = [];
+
+function loadsomeFonts() {
+  fonts[0] = "Orbitron";
+  fonts[1] = "Cascadia";
+  fonts[2] = "Buenard";
+}
+
+async function setup() {
+  createCanvas(640, 480);
+  // Create the webcam video and hide it
+  video = createCapture(VIDEO, {flipped: true});
+  video.size(640, 480);
+  video.hide();
+
+  // load FaceMesh model (p5 v2 method)
+  faceMesh = await ml5.faceMesh(options);
+
+  // Start detecting faces from the webcam video
+  faceMesh.detectStart(video, gotFaces);
+
+  //font loading
+  loadsomeFonts();
+  let font;
   
   for (let i=0; i<numRing; i++) {
-    rings[i] = [];
-    let r = 30 + i * 15; 
+    rings[i] = []; // declaro que en ese Array en ese index haya un array
+    let r = 30 + i * 15; // el radio del círculo que tiene que cambiar según esta fórmula incrementarse
     let circumference = 2 * PI * r;
     let fontSize = 10 + i * 3;
-    let num = floor(circumference / fontSize); // el numero de espacios que queda entre letras disponible
+    font = fonts[i % fonts.length]; // a modulus expresion to make sure that the remainer between the two.. helps you loop throught the array
+    let num = floor(circumference / fontSize); // el número de espacios que queda entre letras disponible
     
     
     for (let j=0; j<num; j++){
@@ -18,19 +49,74 @@ function setup() {
       // para sacar el angulo necesitamos trigonometria    
       let x = width/2 + r * cos(angle);
       let y = height/2 + r * sin(angle);          
-      rings[i].push(new Letter(x, y, fontSize));
+      rings[i].push(new Letter(x, y, fontSize, font)); // fonti tiene que ser un string
     }
   }
 }
 
 function draw() {
 
-  background("steelblue");
+  //background("steelblue");
+  // Draw the webcam video
+  image(video, 0, 0, width, height);
+  
+  if (faces.length > 0 && faces[0].lips) {
+    let topLeftLip = createVector(faces[0].lips.x,
+      faces[0].lips.y);
+    let bottomRightLip = createVector(faces[0].lips.x + faces[0].lips.width,
+        faces[0].lips.y + + faces[0].lips.height);
+    let centerLip = createVector(faces[0].lips.centerX, faces[0].lips.centerY);
+    noFill();
+    stroke(0, 255, 0);
+    // ellipse(topLeftLip.x, topLeftLip.y, 10, 10);
+    // ellipse(bottomRightLip.x, bottomRightLip.y, 10, 10);
+    // ellipse(centerLip.x, centerLip.y, 10, 10);
+    
+    let lipDistance = dist(topLeftLip.x,topLeftLip.y, bottomRightLip.x, bottomRightLip.y);
+    print(lipDistance);
+    
+
+    if (previousLipDistance > 70 && (previousLipDistance - lipDistance > 5)) {
+      for(let i=0; i<rings.length; i++) {
+        for ( let j=0; j<rings[i].length; j++) {
+          let mouth = createVector(centerLip.x, centerLip.y);
+          trigger(rings[i][j], mouth);
+        }
+      } 
+      console.log("triggered")  
+    }
+    previousLipDistance = lipDistance;
+
+
+  }
+
 
   for(let i=0; i<rings.length; i++) {
     for ( let j=0; j<rings[i].length; j++) {
+     
       rings[i][j].update();
       rings[i][j].display();   
     }
   }
+}
+
+
+function trigger(letter, mouth){
+  let force = p5.Vector.sub(letter.position, mouth);
+  let distance = force.mag();
+  force.normalize();
+
+  let magnitude = map(distance, 0, width, 0.1, 1) * random(0.5, 2);
+  force.mult(magnitude);
+  letter.applyForce(force);
+
+  letter.angleV = map(distance, 0, width, 0.01, 0.1) * random(0.5, 2);
+
+}
+
+
+// Callback function for when faceMesh outputs data
+function gotFaces(results) {
+  // Save the output to the faces variable
+  faces = results;
 }
